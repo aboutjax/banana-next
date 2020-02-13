b18b6d8e2e4fd1e5cfa61aa8b5c71d8e830800ce
Jacky Lee <aboutjax1990@gmail.com>
2020-02-13 10:57:17 -0500

    update

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,23 @@
 import React from "react";
+import Link from "next/link";
 import styled from "styled-components";
 
 const Header = styled.h1`
   text-align: center;
 `;
 
+let authUrl = "https://strava-auth.herokuapp.com";
+if (process.env.NODE_ENV === "development") {
+  authUrl = "http://localhost:3000";
+} else {
+  authUrl = "https://strava-auth.herokuapp.com";
+}
+
 const Home = props => {
   return (
     <div>
       <Header>Home</Header>
-      <a href="http://localhost:3000">Login with Strava</a>
+      <a href={authUrl}>Login with Strava</a>
     </div>
   );
 };
