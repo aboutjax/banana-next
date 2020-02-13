fd8515da8386053c37c2007bb1d1eb9ba08544a4
Jacky Lee <aboutjax1990@gmail.com>
2020-02-13 11:36:11 -0500

    update

--- next.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  env: {
-    SECRET: "jacky lee"
-  }
-};
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,15 @@
 import React from "react";
 import Link from "next/link";
 import styled from "styled-components";
-import getConfig from "next/config";
-const { serverRuntimeConfig } = getConfig();
 
 const Header = styled.h1`
   text-align: center;
 `;
 
-let authUrl = "https://strava-auth.herokuapp.com";
-
-if (serverRuntimeConfig) {
-  authUrl = "https://strava-auth.herokuapp.com";
-} else {
-  authUrl = "http://localhost:3000";
-}
-
 const Home = props => {
   return (
     <div>
-      <Header>{process.env.SECRET}</Header>
+      <Header>{process.env.loginurl}</Header>
     </div>
   );
 };
